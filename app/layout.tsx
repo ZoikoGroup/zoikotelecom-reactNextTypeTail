870f173fc3cbbcc191ada8149e1d0007b467d80f
Anowar Sadat <anowar.vista@gmail.com>
2026-06-05 16:47:28 +0530

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Toaster position="top-right" />
         <Header />
+        <Toaster position="top-right" />
         {children}
         <Footer />
         </body>
