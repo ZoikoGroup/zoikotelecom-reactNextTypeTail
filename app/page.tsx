d83ef11cae4370f375cd5d165fbcba24ea79476a
Anowar Sadat <anowar.vista@gmail.com>
2026-06-05 16:59:19 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaArrowRight, FaCheck, FaStar } from "react-icons/fa";
 
-
 type Feature = {
   title: string;
   description: string;
